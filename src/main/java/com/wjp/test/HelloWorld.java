8e45e425d2026a243e26326c827f0e2cff7f2a21
weijupeng <275526034@qq.com>
2020-06-30 16:48:42 +0800

    hello

--- src/main/java/com/wjp/test/HelloWorld.java
+++ src/main/java/com/wjp/test/HelloWorld.java
@@ -7,5 +7,6 @@ package com.wjp.test;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("new Person");
     }
 }
