2e38ad32ce3ea058a9f75a4c59c0f9ea70340987
weijupeng <275526034@qq.com>
2020-06-30 16:47:04 +0800

    hello

---
+++ src/main/java/com/wjp/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.wjp.test;
+
+/**
+ * @author wjp
+ * @date 2020/6/30 16:45
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
